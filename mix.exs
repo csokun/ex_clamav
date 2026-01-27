142d8a6e107b0f175cebaf8f236cf438f962b503
Chorn Sokun <csokun@users.noreply.github.com>
2026-01-27 14:33:14 +1100

    Update license from GPL v2 to GPL-2.0-only

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule ExClamav.MixProject do
       name: "ex_clamav",
       files: ["lib", "mix.exs", "README.md", "Makefile"],
       maintainers: ["Sokun Chorn"],
-      licenses: ["GPL v2"],
+      licenses: ["GPL-2.0-only"],
       links: %{"GitHub" => "https://github.com/csokun/ex_clamav"}
     ]
   end
